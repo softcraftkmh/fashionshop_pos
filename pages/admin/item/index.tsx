b0135363b5357e2af1738e0009bccc11396b5777
Kaung Myat Hein <softcraftkmh@gmail.com>
2019-11-19 09:46:56 +0630

    feat: Add Hook

--- pages/admin/item/index.tsx
+++ pages/admin/item/index.tsx
@@ -4,8 +4,32 @@ import AdminAppBar from "components/AdminAppBar";
 
 import "./index.scss";
 
+interface Item {
+  name: string;
+  price: string;
+  sizes: string;
+  colors: string;
+}
+
 const index = () => {
-  const [{}, setValue] = useState("");
+  const [item, setItem] = useState<Item>({
+    name: "",
+    price: "",
+    sizes: "",
+    colors: ""
+  });
+
+  const { name, price, sizes, colors } = item;
+
+  const onChangeItem = (event, tag) => {
+    const { value } = event.target;
+    setItem({ ...item, [tag]: value });
+  };
+
+  const submitItem = event => {
+    event.preventDefault();
+    console.log(item);
+  };
 
   return (
     <div>
@@ -15,35 +39,54 @@ const index = () => {
           <fieldset>
             <div className="pure-control-group">
               <label htmlFor="name">Item Name</label>
-              <input id="name" type="text" placeholder="Item Name" />
-            </div>
-
-            <div className="pure-control-group">
-              <label htmlFor="password">Password</label>
-              <input id="password" type="password" placeholder="Password" />
-            </div>
-
-            <div className="pure-control-group">
-              <label htmlFor="email">Email Address</label>
-              <input id="email" type="email" placeholder="Email Address" />
-            </div>
-
-            <div className="pure-control-group">
-              <label htmlFor="foo">Supercalifragilistic Label</label>
               <input
-                id="foo"
+                id="name"
                 type="text"
-                placeholder="Enter something here..."
+                placeholder="Item Name"
+                value={name}
+                onChange={event => onChangeItem(event, "name")}
+              />
+            </div>
+
+            <div className="pure-control-group">
+              <label htmlFor="price">Price</label>
+              <input
+                id="price"
+                type="number"
+                placeholder="Price"
+                value={price}
+                onChange={event => onChangeItem(event, "price")}
+              />
+            </div>
+
+            <div className="pure-control-group">
+              <label htmlFor="sizes">Available Sizes</label>
+              <input
+                id="sizes"
+                type="text"
+                placeholder="Sizes"
+                value={sizes}
+                onChange={event => onChangeItem(event, "sizes")}
+              />
+            </div>
+
+            <div className="pure-control-group">
+              <label htmlFor="colors">Available Colors</label>
+              <input
+                id="colors"
+                type="text"
+                placeholder="Colors"
+                value={colors}
+                onChange={event => onChangeItem(event, "colors")}
               />
             </div>
 
             <div className="pure-controls">
-              <label htmlFor="cb" className="pure-checkbox">
-                <input id="cb" type="checkbox" /> I've read the terms and
-                conditions
-              </label>
-
-              <button type="submit" className="pure-button pure-button-primary">
+              <button
+                type="submit"
+                className="pure-button pure-button-primary"
+                onClick={submitItem}
+              >
                 Submit
               </button>
             </div>
